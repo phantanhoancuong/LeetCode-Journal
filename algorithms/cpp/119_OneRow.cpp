106ef85183247787d2c8340b717101593364361e
Phan Tân Hoàn Cương <phantanhoancuong@gmail.com>
2024-02-25 20:31:50 +0700

    C++ Algorithm Optimization - '119. Pascal's Triangle II' with One Row

---
+++ algorithms/cpp/119_OneRow.cpp
@@ -0,0 +1,36 @@
+/* 119. Pascal's Triangle II
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example 1:
+Input: rowIndex = 3
+Output: [1,3,3,1]
+
+Example 2:
+Input: rowIndex = 0
+Output: [1]
+
+Example 3:
+Input: rowIndex = 1
+Output: [1,1]
+ 
+Constraints:
+0 <= rowIndex <= 33
+
+Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space? */
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {                                                   
+        // Initialize row with size rowIndex + 1 and values of 1
+        vector<int> row(rowIndex + 1, 1);
+        for(int i = 1; i <= rowIndex; i++) {
+
+            // Calculate the middle values backwards using combinatorial formula
+            for(int j = i - 1; j > 0; j--) {
+                row[j] += row[j - 1];
+            }
+        }
+        return row;
+    }
+};
