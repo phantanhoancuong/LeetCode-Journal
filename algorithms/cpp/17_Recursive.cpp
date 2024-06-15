c2d7c9a226b40b3f18ddcfe2111fb7a7ce6dc388
Phan Tân Hoàn Cương <phantanhoancuong@gmail.com>
2024-06-15 13:57:53 +0700

    C++ Algorithm - '17. Letter Combinations of a Phone Number'

---
+++ algorithms/cpp/17_Recursive.cpp
@@ -0,0 +1,64 @@
+/* 17. Letter Combinations of a Phone Number
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
+Return the answer in any order.
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Example 2:
+Input: digits = ""
+Output: []
+
+Example 3:
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+Constraints:
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+ */
+
+class Solution {
+public:
+
+    void backtrack(vector<vector<char>>& mappings, string& digits, string currentString, vector<string>& result, int i) {
+        // Base case: currentString is the same length as digits
+        if(i == digits.size()) {
+            result.push_back(currentString);
+            return;
+        }
+
+        // Else, get the next digit and process
+        const vector<char>& letters = mappings[digits[i] - '2'];
+        for(int j = 0; j < letters.size(); j++) {
+            backtrack(mappings, digits, currentString + letters[j], result, i + 1);
+        }
+        
+    }
+    
+    vector<string> letterCombinations(string digits) {
+
+        vector<string> result;
+
+        if(digits.size() == 0) {
+            return result;
+        }
+
+        vector<vector<char>> mappings = {
+            {'a', 'b', 'c'}, 
+            {'d', 'e', 'f'},
+            {'g', 'h', 'i'},
+            {'j', 'k', 'l'},
+            {'m', 'n', 'o'},
+            {'p', 'q', 'r', 's'},
+            {'t', 'u', 'v'},
+            {'w', 'x', 'y', 'z'}
+        };
+
+        backtrack(mappings, digits, "", result, 0);
+        return result;
+    }
+
+};
