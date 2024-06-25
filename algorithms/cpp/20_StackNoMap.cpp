a3d7a755241c62b8143f1b8b78fd8507be4aa924
Phan Tân Hoàn Cương <phantanhoancuong@gmail.com>
2024-06-25 21:38:54 +0700

    C++ Algorithms - '20. Valid Parentheses'
    Added 2 C++ solutions for '20. Valid Parentheses', they use the same logic but one uses a map and the other uses if...else conditionals.

---
+++ algorithms/cpp/20_StackNoMap.cpp
@@ -0,0 +1,52 @@
+/* 20. Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+Constraints:
+1 <= s.length <= 10^4
+s consists of parentheses only '()[]{}'. */
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> parenStack;
+        int n = s.size();
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                parenStack.push(s[i]);
+            }
+            else if(parenStack.empty()) {
+                return false;
+            }
+            else if(s[i] == ')' && parenStack.top() == '(') {
+                parenStack.pop();
+            }
+            else if(s[i] == ']' && parenStack.top() == '[') {
+                parenStack.pop();
+            }
+            else if(s[i] == '}' && parenStack.top() == '{') {
+                parenStack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        
+        return parenStack.empty();
+    }
+};
