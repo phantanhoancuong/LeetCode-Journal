35f1aebce496093511154ea34c87a5b079a509c7
Phan Tân Hoàn Cương <phantanhoancuong@gmail.com>
2024-07-06 20:41:28 +0700

    C++ Algorithms - '1512. Number of Good Pairs'

---
+++ algorithms/cpp/1512_MapTwoLoops.cpp
@@ -0,0 +1,38 @@
+/* 1512. Number of Good Pairs
+Given an array of integers nums, return the number of good pairs.
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+Example 1:
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+Example 2:
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+
+Example 3:
+Input: nums = [1,2,3]
+Output: 0
+
+Constraints:
+1 <= nums.length <= 100
+1 <= nums[i] <= 100 */
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int, int> mappings;
+        int counter = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            mappings[nums[i]]++;
+        }
+        for(auto i = mappings.begin(); i != mappings.end(); i++) {
+            if(i->second > 1) {
+                counter += ((i->second) * (i->second - 1)) / 2;
+            }
+        }
+        return counter;
+    }
+};
