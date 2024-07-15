a5ce976f8bf1f78e070671e31f0cb59a153cad97
Phan Tân Hoàn Cương <phantanhoancuong@gmail.com>
2024-07-15 19:05:35 +0700

    C++ Algorithm - '41. First Missing Positive'

---
+++ algorithms/cpp/41_Swapping.cpp
@@ -0,0 +1,46 @@
+/* 41. First Missing Positive
+Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+Example 1:
+Input: nums = [1,2,0]
+Output: 3
+Explanation: The numbers in the range [1,2] are all in the array.
+
+Example 2:
+Input: nums = [3,4,-1,1]
+Output: 2
+Explanation: 1 is in the array but 2 is missing.
+
+Example 3:
+Input: nums = [7,8,9,11,12]
+Output: 1
+Explanation: The smallest positive integer 1 is missing.
+
+Constraints:
+1 <= nums.length <= 10^5
+-2^31 <= nums[i] <= 2^31 - 1 */
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n = nums.size();
+        // Place each number in its correct position if possible
+        for (int i = 0; i < n; i++) {
+            while (0 < nums[i] && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        // Find the first missing positive
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If no missing positive is found within the range
+        return n + 1;
+    }
+};
