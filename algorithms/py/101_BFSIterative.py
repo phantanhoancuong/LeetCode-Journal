02ecf3f3622e392f334a632f737a887eac343a86
Phan Tân Hoàn Cương <phantanhoancuong@gmail.com>
2025-05-15 12:04:03 +0700

    Python Algorithms - '101. Symmetric Tree'
    - Add both a Python recursive algorithm using DFS and a Python iterative algorithm using BFS and queues for '101. Symmetric Tree'.

---
+++ algorithms/py/101_BFSIterative.py
@@ -0,0 +1,43 @@
+"""
+101. Symmetric Tree
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+
+Follow up: Could you solve it both recursively and iteratively?
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        queue = deque([(root.left, root.right)])
+
+        while queue:
+            node1, node2 = queue.popleft()
+
+            if not node1 and not node2:
+                continue
+
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+
+            queue.append((node1.left, node2.right))
+            queue.append((node1.right, node2.left))
+
+        return True
