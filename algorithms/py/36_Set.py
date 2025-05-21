6b72ef2952e8d90b967aaf67ea6026e25ce2cb3d
Phan Tân Hoàn Cương <phantanhoancuong@gmail.com>
2025-05-21 11:35:43 +0700

    Python Algorithms - '36, Valid Sudoku'
    - Add both cell-wise validation and set-based approach for '36. Valid Sudoku' in Python

---
+++ algorithms/py/36_Set.py
@@ -0,0 +1,67 @@
+"""
+36. Valid Sudoku
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+Input: board =
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+Constraints:
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+"""
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_sets = [set() for _ in range(9)]
+        col_sets = [set() for _ in range(9)]
+        box_sets = [set() for _ in range(9)]
+        for i in range(9):
+            for j in range(9):
+                box_index = (i // 3) * 3 + (j // 3)
+                cur_val = board[i][j]
+                if cur_val == ".":
+                    continue
+                if (
+                    cur_val in row_sets[i]
+                    or cur_val in col_sets[j]
+                    or cur_val in box_sets[box_index]
+                ):
+                    return False
+                row_sets[i].add(cur_val)
+                col_sets[j].add(cur_val)
+                box_sets[box_index].add(cur_val)
+
+        return True
